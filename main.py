21fd8024759c9d2598cd74a79937f4910be0b84e
liammoroney <liam.moroney14@gmail.com>
2019-01-19 10:57:28 +0000

    Added try catch for handling youtubedl exceptions and retrying. Added another thread so that the view can be rendered sooner before the downloading begins. Added cleanup of tracks which aren't mp3's (leftover parts from previous downloads). Fixed logic for getting missing playlist tracks.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import eyed3
 import requests
 import urllib
 import youtube_dl
-from os import listdir, mkdir
+from os import listdir, mkdir, listdir, remove
 from os.path import isfile, isdir, join
 from flask import Flask, request, redirect, g, render_template
 from flask_socketio import SocketIO, emit
@@ -24,7 +24,8 @@ SPOTIFY_ACCESS_TOKEN = None
 
 YOUTUBE_KEY_INDEX = 0
 
-thread = None
+update_thread = None
+main_thread = None
 playlists_status = {}
 
 @socketio.on('connect', namespace='/test')
@@ -66,7 +67,7 @@ def download_spotify_track(track, playlist_name):
 
     r = None
     while (r == None or r.status_code != 200) and YOUTUBE_KEY_INDEX < len(YOUTUBE_KEYS):
-        youtube_params = {
+        youtube_search_params = {
         'part': 'snippet',
         'type': 'video',
         'maxResults': 1,
@@ -75,7 +76,7 @@ def download_spotify_track(track, playlist_name):
         }
 
         r = requests.get(
-        'https://www.googleapis.com/youtube/v3/search', params=youtube_params)
+        'https://www.googleapis.com/youtube/v3/search', params=youtube_search_params)
 
         # Try next key
         YOUTUBE_KEY_INDEX += 1
@@ -96,28 +97,41 @@ def download_spotify_track(track, playlist_name):
             'outtmpl': 'Playlists/{0}/{1}.%(ext)s'.format(playlist_name, track_name)
         }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+        attempts = 0
 
-        # Get album art
-        image_url = track['track']['album']['images'][0]['url']
-        image = urllib.request.urlopen(image_url).read()
+        while attempts < 4:
+            try:
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([url])
+                # Check if file downloaded and converted successfully
+                if isfile(file_path):
+                    break
+            except e:
+                print("Youtube download error: {0}".format(e))
 
-        # Add id3 tags
-        audiofile = eyed3.load(file_path)
-        audiofile.tag.artist = artist
-        audiofile.tag.title = track_name
-        audiofile.tag.images.set(3, image , "image/jpeg" ,u"Description")
-        
-        audiofile.tag.save()
+        # Check if file downloaded and converted successfully
+        if isfile(file_path):
+            # Get album art
+            image_url = track['track']['album']['images'][0]['url']
+            image = urllib.request.urlopen(image_url).read()
 
-        # Move from missing to downloaded tracks list
-        playlists_status[playlist_name]['missing'].remove(track_name)
-        playlists_status[playlist_name]['downloaded'].append(track_name)
+            # Add id3 tags
+            audiofile = eyed3.load(file_path)
+            audiofile.tag.artist = artist
+            audiofile.tag.title = track_name
+            audiofile.tag.images.set(3, image , "image/jpeg" ,u"Description")
+            
+            audiofile.tag.save()
+
+            # Move from missing to downloaded tracks list
+            playlists_status[playlist_name]['missing'].remove(track_name)
+            playlists_status[playlist_name]['downloaded'].append(track_name)
+        else:
+            print("Track did not download/convert successfully")
     else:
         print('tried all keys')
         print('youtube response error')
-        print(r.json()['error']['message'])
+        print(r.json())
 
 
 def load_config():
@@ -132,7 +146,7 @@ def load_config():
 
 def update_page():
     while True:
-        time.sleep(5)
+        time.sleep(1)
         socketio.emit('message', {'playlists_status': json.dumps(playlists_status)}, namespace='/test')
 
 @app.route('/')
@@ -143,65 +157,80 @@ def index():
 
 # Make sure file name doesn't contain any illegal characters
 def sanitise_file_name(name):
-    return name.replace('/','-').strip()
+    return name.replace('/','-').strip('.')
 
 @app.route('/callback/q')
 def callback():
-    global thread, playlists_status
+    global update_thread, main_thread
+
     authorization_header = user_authorisation(
         SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
 
-    profile = profile_data(authorization_header)
-    Name = profile["display_name"]
-    external_urls = profile["external_urls"]
-    uri = profile["uri"]
-    href = profile["href"]
-    id = profile["id"]
-
-    playlists = user_playlist_data(authorization_header, profile)
-
-    threads = []
-
-    if thread is None:
-        thread = threading.Thread(target=update_page)
-        thread.start()
-
-    for item in playlists["items"]:
-
-        playlist = playlist_data(authorization_header, item['id'])
-        playlist_name = item['name']
-
-        # Only download wanted playlists
-        if (playlist_name not in SPOTIFY_PLAYLISTS):
-            continue
-
-        playlist_path = 'Playlists/{0}/'.format(playlist_name)
-
-        # Create playlist folder if it doens't exist
-        if not isdir(playlist_path):
-            mkdir(playlist_path)
-
-        # Get current and playlist track names
-        current_track_names = sorted([f.replace('.mp3','') for f in listdir(playlist_path) if isfile(join(playlist_path, f))], key=str.lower)
-        playlist_track_names = sorted([sanitise_file_name(f['track']['name']) for f in playlist['tracks']['items']], key=str.lower)
-
-        # Return playlist tracks which aren't downloaded
-        missing_tracks = list(set(current_track_names) ^ set(playlist_track_names))
-        print('Missing these {0} tracks from {1}'.format(len(missing_tracks), playlist_name))
-        print(missing_tracks)
-
-        playlists_status[playlist_name] = {'missing': missing_tracks, 'downloaded': current_track_names}
-
-        for track in playlist['tracks']['items']:
-            if sanitise_file_name(track['track']['name']) in missing_tracks:
-                thread = threading.Thread(
-                    target=download_spotify_track, args=(track, playlist_name))
-                thread.start()
-
-    for thread in threads:
-        thread.join()
+    if update_thread is None:
+        update_thread = threading.Thread(target=update_page)
+        update_thread.start()
+    if main_thread is None:
+        main_thread = threading.Thread(target=get_tracks, args=(authorization_header,))
+        main_thread.start()
 
     return render_template('index.html', playlists=playlists_status)
+
+def get_tracks(authorization_header):
+    global thread, playlists_status
+
+    with app.test_request_context():
+
+        profile = profile_data(authorization_header)
+        Name = profile["display_name"]
+        external_urls = profile["external_urls"]
+        uri = profile["uri"]
+        href = profile["href"]
+        id = profile["id"]
+
+        playlists = user_playlist_data(authorization_header, profile)
+
+        threads = []
+
+        for item in playlists["items"]:
+
+            playlist = playlist_data(authorization_header, item['id'])
+            playlist_name = item['name']
+
+            # Only download wanted playlists
+            if (playlist_name not in SPOTIFY_PLAYLISTS):
+                continue
+
+            playlist_path = 'Playlists/{0}/'.format(playlist_name)
+
+            # Create playlist folder if it doens't exist
+            if not isdir(playlist_path):
+                mkdir(playlist_path)
+            else:
+                # Cleanup .webm's
+                current_tracks = listdir(playlist_path)
+                for track in current_tracks:
+                    if track.endswith(".webm") or track.endswith(".m4a"):
+                        remove(join(playlist_path, track))
+
+            # Get current and playlist track names
+            current_track_names = sorted([f.replace('.mp3','') for f in listdir(playlist_path) if isfile(join(playlist_path, f))], key=str.lower)
+            playlist_track_names = sorted([sanitise_file_name(f['track']['name']) for f in playlist['tracks']['items']], key=str.lower)
+
+            # Return playlist tracks which aren't downloaded
+            missing_tracks = list(set(playlist_track_names).difference(current_track_names))
+            print('Missing these {0} tracks from {1}'.format(len(missing_tracks), playlist_name))
+            print(missing_tracks)
+
+            playlists_status[playlist_name] = {'missing': missing_tracks, 'downloaded': current_track_names}
+
+            for track in playlist['tracks']['items']:
+                if sanitise_file_name(track['track']['name']) in missing_tracks:
+                    thread = threading.Thread(
+                        target=download_spotify_track, args=(track, playlist_name))
+                    thread.start()
+
+        for thread in threads:
+            thread.join()
 
 
 if __name__ == '__main__':
